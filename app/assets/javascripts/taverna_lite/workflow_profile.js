903ecaabd966e558a054648415feba667a4d61fe
Abraham Nieva <a.nieva@cs.cardiff.ac.uk>
2013-09-20 17:27:46 +0100

    04 show processor details when selected

--- app/assets/javascripts/taverna_lite/workflow_profile.js
+++ app/assets/javascripts/taverna_lite/workflow_profile.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+  $(function() {
+    $( "#selectable" ).selectable({
+      stop: function() {
+        var result = $( "#select-result" ).empty();
+        $( ".ui-selected", this ).each(function() {
+          var index = $( "#selectable li" ).index( this );
+          var namex = $(this).attr('id')
+          result.append( namex );
+        });
+      }
+    });
+  });
