681975c5deb353e07eaeb71886809589c729e138
Abraham Nieva <a.nieva@cs.cardiff.ac.uk>
2013-10-11 17:04:58 +0100

    created test suite for t2flow writer

---
+++ test/unit/taverna_lite/t2flow_writer_test.rb
@@ -0,0 +1,64 @@
+# Copyright (c) 2012-2013 Cardiff University, UK.
+# Copyright (c) 2012-2013 The University of Manchester, UK.
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the names of The University of Manchester nor Cardiff University nor
+#   the names of its contributors may be used to endorse or promote products
+#   derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Authors
+#     Abraham Nieva de la Hidalga
+#
+# Synopsis
+#
+# BioVeL Taverna Lite is a prototype interface provided to support the
+# inspection and modification of workflows.
+#
+# For more details see http://www.biovel.eu
+#
+# BioVeL is funded by the European Commission 7th Framework Programme (FP7),
+# through the grant agreement number 283359.
+require 'test_helper'
+
+module TavernaLite
+  class PortTypeTest < ActiveSupport::TestCase
+    # test saving workflow annotations, need to pass a valid workflow file and
+    # values for name, description, title and author
+    # will call t2flow to read and validate that save workflow is t2flow
+    test "the truth" do
+      # need a set up with the value of the workflow file path
+      # copy the file so that it can be used in different tests
+      # modify the file by writing annotations
+      # verify that the file is t2flow
+      # verify that the file values for name,author, description and title are
+      # the same as those passed as parameters
+      # writer = T2flowWriter.new
+      # writer.save_wf_annotations(xmlFile, author, description, title, name)
+      assert true
+      assert true
+    end
+  end
+end
