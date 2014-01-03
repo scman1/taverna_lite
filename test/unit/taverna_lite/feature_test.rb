6e065c17f6ff166f6dead39830148498ab8b05ba
Abraham Nieva <a.nieva@cs.cardiff.ac.uk>
2014-01-03 16:22:30 +0000

    Enabling remove processor UI 09 - testing feature tree

--- test/unit/taverna_lite/feature_test.rb
+++ test/unit/taverna_lite/feature_test.rb
@@ -1,9 +1,99 @@
+# Copyright (c) 2012-2013 Cardiff University, UK.
+# Copyright (c) 2012-2013 The University of Manchester, UK.
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the names of The University of Manchester nor Cardiff University nor
+#   the names of its contributors may be used to endorse or promote products
+#   derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Authors
+#     Abraham Nieva de la Hidalga
+#
+# Synopsis
+#
+# BioVeL Taverna Lite is a prototype interface provided to support the
+# inspection and modification of workflows.
+#
+# For more details see http://www.biovel.eu
+#
+# BioVeL is funded by the European Commission 7th Framework Programme (FP7),
+# through the grant agreement number 283359.
 require 'test_helper'
 
 module TavernaLite
   class FeatureTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    setup do
+      @root = taverna_lite_features(:one)
+      # get stage matrix is mandatory and has 1 mandatory and 2 xor children
+      @getStageMatrix = taverna_lite_features(:two)
+      # analyse matrix is mandatory and has 6 or children
+      @analyseMatrix = taverna_lite_features(:twentyseven)
+      # read plain text is xor and has 5 optional children
+      @readPlainText = taverna_lite_features(:nine)
+      # text table to R is optional, has no children and its instantiated
+      @textTableToR = taverna_lite_features(:twelve)
+    end
+
+    test "00 get parent for root" do
+      assert_nil @root.parent
+    end
+    test "01 get parent for node" do
+      assert_equal @getStageMatrix.parent, @root
+    end
+    test "02 get mandatory features" do
+      x = @getStageMatrix.mandatory
+      assert_equal 1, x.length
+      assert_equal 2, x[0].feature_type_id
+    end
+    test "03 get xor features" do
+      x = @getStageMatrix.xor
+      assert_equal 2, x.length
+      x.each { |ft|
+        assert_equal 3, ft.feature_type_id
+      }
+    end
+    test "04 get or features" do
+      x = @analyseMatrix.or
+      assert_equal 6, x.length
+      x.each { |ft|
+        assert_equal 4, ft.feature_type_id
+      }
+    end
+    test "05 get optional features" do
+      x = @readPlainText.optional
+      assert_equal 5, x.count
+      x.each { |ft|
+        assert_equal 5, ft.feature_type_id
+      }
+    end
+    test "06 test if feature is instantiated" do
+      assert @textTableToR.instantiated
+      assert_not_nil @textTableToR.component_id
+      assert !@readPlainText.instantiated
+      assert_equal 0, @readPlainText.component_id
+    end
   end
 end
